6a8a689bca74c51c91912405533b26584a93ba04
Nickchooshin <gustncjswp95@gmail.com>
2015-10-14 14:29:41 +0900

    Add Scenes(title, game)

---
+++ Assets/Resources/Scripts/Game/CircleBeat.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleBeat : MonoBehaviour {
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+}
---
+++ Assets/Resources/Scripts/Title/Title.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+    
+    void Start()
+    {
+    }
+	
+    void Update()
+    {
+        // Mobile
+        if (Input.touchCount > 0)
+            Application.LoadLevel("sceneGame");
+
+        // PC
+        /*
+        if (Input.GetMouseButtonDown(0))
+            Application.LoadLevel("sceneGame");
+        */
+	}
+}
