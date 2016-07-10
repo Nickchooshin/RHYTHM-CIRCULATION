8350c059104eeba26d50d8636b54c34845d4c29a
Nickchooshin <gustncjswp95@gmail.com>
2016-07-10 14:38:54 +0900

    added ButtonFadeInOut, Credit

---
+++ Assets/Resources/Scripts/ButtonFadeInOut.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonFadeInOut : MonoBehaviour
+{
+    public Image buttonImage;
+    public float fadeInOutTime = 1.0f;
+
+    void Start()
+    {
+        StartCoroutine("FadeInOut");
+    }
+
+    private IEnumerator FadeInOut()
+    {
+        float endTime = Time.time + fadeInOutTime;
+        bool isFadeIn = true;
+
+        while (true)
+        {
+            float alpha = ((endTime - Time.time) / fadeInOutTime);
+
+            if (alpha < 0.0f)
+            {
+                endTime += fadeInOutTime;
+                isFadeIn = !isFadeIn;
+                alpha = 1.0f - alpha;
+            }
+
+            if (isFadeIn)
+                buttonImage.color = new Color(1.0f, 1.0f, 1.0f, 1.0f - alpha);
+            else
+                buttonImage.color = new Color(1.0f, 1.0f, 1.0f, alpha);
+
+            yield return null;
+        }
+    }
+}
